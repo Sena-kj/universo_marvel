071e22d49efc2159b07ed8016de6c7bf5df87e7d
Sena-kj <kauansenna1233@gmail.com>
2025-06-15 22:34:25 -0300

    Update lock.php

--- lock.php
+++ lock.php
@@ -5,10 +5,8 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-// Verifica se o ID do usuário está definido na sessão
-// Se não estiver logado, redireciona para a página de login com código de acesso não autorizado
 if (!isset($_SESSION['usuario_id'])) {
     header('location:index.php?codigo=1'); // Código 1: acesso não autorizado
-    exit(); // É crucial chamar exit() após um redirecionamento
+    exit(); 
 }
 ?>
